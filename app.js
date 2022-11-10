1399a1079f00d53e25a9e96f0d9ddee73a899b56
sander-aleks <msanderaleks@gmail.com>
2022-11-10 18:10:08 +0200

    progress

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+    var x = document.getElementById("Links");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
